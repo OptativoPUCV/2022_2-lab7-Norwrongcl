f37b6b0c0dca9268b6b903f11cc79019d06e8d34
Replit user <>
2022-09-13 02:14:29 +0000

    new try

--- heap.c
+++ heap.c
@@ -43,16 +43,20 @@ void heap_push(Heap* pq, void* data, int priority){
   pq->size++;
 }
 
-void heap_pop(Heap* pq){
+/*void heap_pop(Heap* pq){
   heapElem* aux= (heapElem*)malloc(sizeof(heapElem));
+	
   pq->heapArray[0]= pq->heapArray[pq->size];
   pq->size--;
+	
   int asize= 0;
   int younga= (2*asize)+ 1;
-  int olda= (2*asize)+ 2; 
+  int olda= (2*asize)+ 2;
+	
   while (asize < pq->size){
     if (pq->heapArray[asize].priority < pq->heapArray[younga].priority && pq->heapArray[younga].priority > pq->heapArray[olda].priority){
-      *aux= pq->heapArray[asize];
+      
+			*aux= pq->heapArray[asize];
       pq->heapArray[asize]= pq->heapArray[younga];
       pq->heapArray[younga]= *aux;
       asize= younga;
@@ -60,7 +64,8 @@ void heap_pop(Heap* pq){
       olda= (2*asize)+ 2;
     }
     else if (pq->heapArray[asize].priority < pq->heapArray[olda].priority && pq->heapArray[olda].priority > pq->heapArray[younga].priority){
-      *aux= pq->heapArray[asize];
+      
+			*aux= pq->heapArray[asize];
       pq->heapArray[asize]= pq->heapArray[olda];
       pq->heapArray[olda]= *aux;
 
@@ -69,6 +74,41 @@ void heap_pop(Heap* pq){
       olda= (2*asize)+ 2;
     }
     else break;
+   }
+}*/
+void heap_pop(Heap* pq){
+   heapElem* aux = (heapElem*) malloc (sizeof(heapElem));
+   
+   pq->heapArray[0] = pq->heapArray[pq->size];
+   pq->size--;
+
+   int auxSize = 0;
+   int youngerAux = (2*auxSize) + 1;
+   int olderAux = (2*auxSize) + 2; 
+   while (auxSize < pq->size)
+   {
+      if (pq->heapArray[auxSize].priority < pq->heapArray[youngerAux].priority && pq->heapArray[youngerAux].priority > pq->heapArray[olderAux].priority)
+      {
+         *aux = pq->heapArray[auxSize];
+         pq->heapArray[auxSize] = pq->heapArray[youngerAux];
+         pq->heapArray[youngerAux] = *aux;
+
+         auxSize = youngerAux;
+         youngerAux = (2*auxSize) + 1;
+         olderAux = (2*auxSize) + 2;
+      }
+      else if (pq->heapArray[auxSize].priority < pq->heapArray[olderAux].priority && pq->heapArray[olderAux].priority > pq->heapArray[youngerAux].priority)
+      {
+         *aux = pq->heapArray[auxSize];
+         pq->heapArray[auxSize] = pq->heapArray[olderAux];
+         pq->heapArray[olderAux] = *aux;
+
+         auxSize = olderAux;
+         youngerAux = (2*auxSize) + 1;
+         olderAux = (2*auxSize) + 2;
+      }
+      else
+         break;
    }
 }
 
