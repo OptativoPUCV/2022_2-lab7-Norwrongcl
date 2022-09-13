3afb8fc794b9fcd49a5e48b386910b018d4d0f88
Replit user <>
2022-09-13 01:45:14 +0000

    new try

--- heap.c
+++ heap.c
@@ -23,7 +23,7 @@ void* heap_top(Heap* pq){
 
 void heap_push(Heap* pq, void* data, int priority){
   if (pq->size == pq->capac){
-    pq->capac= (pq->capac*2)+1;
+    pq->capac= (pq->capac*2)+ 1;
     pq->heapArray= realloc(pq->heapArray, pq->capac*sizeof(heapElem));
   }
   pq->heapArray[pq->size].data= data;
@@ -44,7 +44,32 @@ void heap_push(Heap* pq, void* data, int priority){
 }
 
 void heap_pop(Heap* pq){
+  heapElem* aux= (heapElem*)malloc(sizeof(heapElem));
+  pq->heapArray[0]= pq->heapArray[pq->size];
+  pq->size--;
+  int asize= 0;
+  int younga= (2*asize)+ 1;
+  int olda= (2*asize)+ 2; 
+  while (asize < pq->size){
+    if (pq->heapArray[asize].priority < pq->heapArray[youngax].priority && pq->heapArray[younga].priority > pq->heapArray[olda].priority){
+      *aux= pq->heapArray[asize];
+      pq->heapArray[asize]= pq->heapArray[younga];
+      pq->heapArray[younga]= *aux;
+      asize= younga;
+      younga= (2*asize)+ 1;
+      olda= (2*asize)+ 2;
+    }
+    else if (pq->heapArray[asize].priority < pq->heapArray[olda].priority && pq->heapArray[olda].priority > pq->heapArray[younga].priority){
+      *aux= pq->heapArray[asize];
+      pq->heapArray[asize]= pq->heapArray[olda];
+      pq->heapArray[olda]= *aux;
 
+      asize= olda;
+      younga= (2*asize)+ 1;
+      olda= (2*asize)+ 2;
+    }
+    else break;
+   }
 }
 
 Heap* createHeap(){
